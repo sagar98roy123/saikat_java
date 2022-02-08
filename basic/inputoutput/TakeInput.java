4b3c7a04a03d926e9f093b7add09bbcaf2ea7cc7
Sagar <sagar98roy123@gmail.com>
2022-02-09 00:38:11 +0530

    how to take input

---
+++ basic/inputoutput/TakeInput.java
@@ -0,0 +1,22 @@
+package saikat.java.basic.inputoutput;
+
+import java.util.Scanner;
+
+public class TakeInput {
+
+	public static void main(String[] args) {
+		
+		int a;
+		
+		Scanner sc =new Scanner(System.in);
+		
+		System.out.println("Enter the value of a:");
+		
+		a=sc.nextInt();
+		
+		System.out.println("The value of a is "+ a);
+		
+
+	}
+
+}
